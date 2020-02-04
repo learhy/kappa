bf1aa2cd05787c5b8eb83c747a5a1cf54fbd2d7f
Will <will@glozer.net>
2020-02-04 14:10:24 +0900

    retain separate socket->process map

--- src/combine/combine.rs
+++ src/combine/combine.rs
@@ -1,42 +1,64 @@
 use std::collections::HashMap;
+use std::mem;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::mem;
+use std::time::{Duration, Instant};
 use anyhow::Result;
 use log::debug;
 use parking_lot::Mutex;
 use kentik_api::{Client, Device};
-use crate::capture::flow::Key;
+use crate::capture::flow::{Addr, Key};
 use crate::collect::Record;
 use crate::export::{pack, send};
+use crate::sockets::Process;
 
 pub struct Combine {
-    queue:  Mutex<HashMap<Key, Record>>,
-    empty:  Mutex<HashMap<Key, Record>>,
-    client: Arc<Client>,
-    device: Arc<Device>,
-    dump:   AtomicBool,
+    queue:   Mutex<HashMap<Key, Record>>,
+    empty:   Mutex<HashMap<Key, Record>>,
+    procs:   Mutex<HashMap<Addr, Proc>>,
+    client:  Arc<Client>,
+    device:  Arc<Device>,
+    dump:    AtomicBool,
+    timeout: Duration,
+}
+
+pub struct Proc {
+    proc: Arc<Process>,
+    seen: Instant,
 }
 
 impl Combine {
     pub fn new(client: Arc<Client>, device: Device) -> Self {
         Self {
-            queue:  Mutex::new(HashMap::new()),
-            empty:  Mutex::new(HashMap::new()),
-            client: client,
-            device: Arc::new(device),
-            dump:   AtomicBool::new(false),
+            queue:   Mutex::new(HashMap::new()),
+            empty:   Mutex::new(HashMap::new()),
+            procs:   Mutex::new(HashMap::new()),
+            client:  client,
+            device:  Arc::new(device),
+            dump:    AtomicBool::new(false),
+            timeout: Duration::from_secs(60),
         }
     }
 
     pub fn combine(&self, rs: Vec<Record>) {
         let mut queue = self.queue.lock();
+        let mut procs = self.procs.lock();
+
+        let now = Instant::now();
+
+        let mut set = |addr: Addr, p: &Arc<Process>| {
+            procs.insert(addr, Proc {
+                proc: p.clone(),
+                seen: now,
+            });
+        };
+
         for r in rs {
+            r.src.as_ref().map(|p| set(r.flow.src, p));
+            r.dst.as_ref().map(|p| set(r.flow.dst, p));
             queue.entry(r.flow.key()).and_modify(|entry| {
                 entry.flow.bytes   += r.flow.bytes;
                 entry.flow.packets += r.flow.packets;
-                r.src.as_ref().map(|p| entry.src = Some(p.clone()));
-                r.dst.as_ref().map(|p| entry.dst = Some(p.clone()));
             }).or_insert(r);
         }
     }
@@ -44,9 +66,15 @@ impl Combine {
     pub fn export(&self) -> Result<()> {
         let mut queue  = self.queue.lock();
         let mut export = self.empty.lock();
+        let mut procs  = self.procs.lock();
 
         mem::swap(&mut *queue, &mut *export);
         drop(queue);
+
+        for r in &mut export.values_mut() {
+            r.src = procs.get(&r.flow.src).map(|p| p.proc.clone());
+            r.dst = procs.get(&r.flow.dst).map(|p| p.proc.clone());
+        }
 
         if self.dump.load(Ordering::SeqCst) {
             debug!("combine state:");
@@ -59,6 +87,9 @@ impl Combine {
         let client = self.client.clone();
         let device = self.device.clone();
         tokio::spawn(send(client, device, msg));
+
+        let now = Instant::now();
+        procs.retain(|_, p| now - p.seen < self.timeout);
 
         Ok(())
     }
